5c58d286089f9a289f4c3716d9c43b38dc251a78
david mallon <mallond@gmail.com>
2017-05-28 17:52:00 -0500

    add required field edit

--- js/dapp.js
+++ js/dapp.js
@@ -18,42 +18,10 @@ function handleInit(err) {
   }
   node.start(function () {
     console.log('Online status: ', node.isOnline() ? 'online' : 'offline');
-    document.getElementById("status").innerHTML = 'IPFS status: ' + (node.isOnline() ? 'Online and running in the browser' : 'offline');
+    document.getElementById("status").innerHTML = 'IPFS status: ' + (node.isOnline() ? 'Online' : 'offline');
     // You can write more code here to use it. Use methods like
     // node.files.add, node.files.get. See the API docs here:
     // https://github.com/ipfs/interface-ipfs-core/tree/master/API
-    var file = '';
-    node.files.add(new node.types.Buffer('Hello world! from moto'), function (err, res) {
-      if (err || !res) {
-        return console.error('Error - ipfs files add', err, res)
-      }
-
-      res.forEach(function (file) {
-        console.log('successfully stored', file);
-
-
-        node.files.cat(file.hash, function (err, stream) {
-          var res = '';
-
-          stream.on('data', function (chunk) {
-            res += chunk.toString()
-          });
-
-          stream.on('error', function (err) {
-            console.error('Error - ipfs files cat ', err)
-          });
-
-          stream.on('end', function () {
-            console.log('Got:', res)
-          })
-        })
-
-      })
-    });
-
-
-
-
 
     setInterval(function() {
       node.swarm.addrs(function (err, addrs) {
