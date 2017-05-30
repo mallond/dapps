fe0ba792506a9799fa19bfacb5266807f00f0583
david mallon <mallond@gmail.com>
2017-05-29 21:51:03 -0500

    chrome tested only

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -26447,25 +26447,21 @@ module.exports = function(module) {
 
 
 
-const bitcore = __webpack_require__(58);
-const Message = __webpack_require__(155);
+var bitcore = __webpack_require__(58);
+var Message = __webpack_require__(155);
 
 
-const privateKey = bitcore.PrivateKey.fromWIF('L3yk3ZLnGvqaEeYyJRkzL3T9NPRqATNpe34KPtLTQLX1RdeunYiM');
-const data = 'tada';
-const signature = Message(data).sign(privateKey);
-const address = '135eGSTRjQcK6K9Gyqa2MsHpvnxgtdLMqG';
-var verified = Message(data).verify(address, signature);
-
-console.log(verified);
-
-const getSignature = (privateKey, data)=>{
-  const _privateKey = bitcore.PrivateKey.fromWIF(privateKey);
-  const signature = Message(data).sign(_privateKey);
-  return signature;
+var getSignature = function(privateKey, data) {
+  try {
+    var _privateKey = bitcore.PrivateKey.fromWIF(privateKey);
+    var signature = Message(data).sign(_privateKey);
+    return signature;
+  } catch (error) {
+    alert(error);
+  }
 };
 
-const verify = (address, signature, data)=>{
+var verify = function(address, signature, data) {
   var verified = Message(data).verify(address, signature);
   return verified;
 };
@@ -26473,10 +26469,6 @@ const verify = (address, signature, data)=>{
 if (!window.i) {
   window.DAPPLog={};
 }
-
-window.DAPPLog.tada = ()=>{
-  console.log('tada tada tada');
-};
 
 window.DAPPLog.getSignature = getSignature;
 window.DAPPLog.verify = verify;
